6699c135260d68b286ded58392fc760adae05282
MultiBIMLord <116342974+MultiBIMLord@users.noreply.github.com>
2023-09-27 19:29:33 +0200

    Update AssemblyInfo.cs

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Valhalla")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Dynamo Zero Touch - specific for Norway")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Multiconsult")]
+[assembly: AssemblyCompany("BIMLord")]
 [assembly: AssemblyProduct("Valhalla")]
-[assembly: AssemblyCopyright("Copyright © Multiconsult 2023")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("OpenSource/MIT")]
+[assembly: AssemblyTrademark("BimseMumsen")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2023.09.27.0")]
+[assembly: AssemblyFileVersion("2023.09.27.0")]
